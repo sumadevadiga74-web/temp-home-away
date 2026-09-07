28b39c8136d170f3457085d8fff7ad44d26afb0f
Suma Devadiga <your-github-email@example.com>
2026-09-07 17:03:08 +0530

    Fix theme hydration mismatch

--- components/navbar/DarkMode.tsx
+++ components/navbar/DarkMode.tsx
@@ -13,37 +13,50 @@ import {
 } from '@/components/ui/dropdown-menu';
 
 export default function ModeToggle() {
-const { resolvedTheme, setTheme } = useTheme();
+  const { resolvedTheme, setTheme } = useTheme();
+  const [mounted, setMounted] = React.useState(false);
+
+  React.useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
   return (
     <DropdownMenu>
-     <DropdownMenuTrigger asChild>
-  <Button variant="outline" size="icon">
-    <SunIcon
-      className={`h-[1.2rem] w-[1.2rem] transition-all ${
-        resolvedTheme === "dark"
-          ? "-rotate-90 scale-0"
-          : "rotate-0 scale-100"
-      }`}
-    />
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon">
+          <SunIcon
+            className={`h-[1.2rem] w-[1.2rem] transition-all ${
+              resolvedTheme === 'dark'
+                ? '-rotate-90 scale-0'
+                : 'rotate-0 scale-100'
+            }`}
+          />
 
-    <MoonIcon
-      className={`absolute h-[1.2rem] w-[1.2rem] transition-all ${
-        resolvedTheme === "dark"
-          ? "rotate-0 scale-100"
-          : "rotate-90 scale-0"
-      }`}
-    />
+          <MoonIcon
+            className={`absolute h-[1.2rem] w-[1.2rem] transition-all ${
+              resolvedTheme === 'dark'
+                ? 'rotate-0 scale-100'
+                : 'rotate-90 scale-0'
+            }`}
+          />
 
-    <span className="sr-only">Toggle theme</span>
-  </Button>
-</DropdownMenuTrigger>
-      <DropdownMenuContent align='end'>
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent align="end">
         <DropdownMenuItem onClick={() => setTheme('light')}>
           Light
         </DropdownMenuItem>
+
         <DropdownMenuItem onClick={() => setTheme('dark')}>
           Dark
         </DropdownMenuItem>
+
         <DropdownMenuItem onClick={() => setTheme('system')}>
           System
         </DropdownMenuItem>
